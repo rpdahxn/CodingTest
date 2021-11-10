cf3cfd5e297420eb513e01a0010f2891eb25eda4
Dahyeon Kye <rpekgus@gmail.com>
2021-11-11 01:02:45 +0900

    [BOJ] 11653 소인수분해

--- mathematics/[BOJ] 11653.py
+++ mathematics/[BOJ] 11653.py
@@ -1,26 +1,34 @@
-# 내코드
-# 소수를 구해야 한다고 생각했다..
-
-# def isPrime(n):
-#     count=0
-#     for i in range(1, n+1):
-#         if n%i==0:
-#             count+=1
-#     if count==2:
-#         return True
+# 정답 1
+# N = int(input())
+# m = 2
+# while N != 1:
+#     if N % m == 0:
+#         print(m)
+#         N = N // m
 #     else:
-#         return False
+#         m += 1
 
-# n = int(input())
-# 약수
-#
+# 정답 2
+n = int(input())
+i = 2
+r = int(n ** 0.5)
 
-# 정답코드
-N = int(input())
-m = 2
-while N != 1:
-    if N % m == 0:
-        print(m)
-        N = N // m
-    else:
-        m += 1
+while i <= r:
+    while not n % i:
+        print(i)
+        n //= i
+    i += 1
+if n > 1:
+    print(n)
+
+# 내 코드
+# N = int(input())
+# m = 2
+# while True:
+#     if N % m == 0:
+#         N //= m
+#         print(m)
+#         if N == 1:
+#             break
+#     else:
+#         m += 1
